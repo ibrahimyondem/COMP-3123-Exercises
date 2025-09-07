7948391fb2716d0418d5f74745710622fa5cedef
Ibrahim Yondem <ibrahim.y2001@gmail.com>
2025-09-06 22:29:35 -0400

    Exercise 3

--- lab1.js
+++ lab1.js
@@ -30,3 +30,27 @@ console.log(max(0,-10,-20));
 console.log(max(1000,510,440));
 
 //EXERCISE 3
+function findAngle(degree){
+    if(degree < 0 && degree>360){
+        return "Degree should be less than 360 and more than 0";
+    }
+
+    if(degree === 0 || degree === 360 || degree === 180){
+        return "Straight angle";
+    }
+        else if(degree > 0 && degree <90){
+            return "Acute angle";
+        }
+        else if(degree === 90){r
+            return "Right angle"
+        }
+        else if(degree > 90 && degree < 180){
+            return "Obtuse angle";
+        }
+        else if()
+    }
+
+console.log(findAngle(47))
+console.log(findAngle(90))
+console.log(findAngle(145))
+console.log(findAngle(180))
