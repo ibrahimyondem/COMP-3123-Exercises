e9a6a9fc9c8c121ceff376c7c639d57d5ae3a0c2
Ibrahim Yondem <ibrahim.y2001@gmail.com>
2025-09-06 22:20:19 -0400

    Exercise 2

--- lab1.js
+++ lab1.js
@@ -14,4 +14,19 @@ console.log(capitalizeWords("the quick brown fox"));
 
 // EXERCISE 2
 
+function max(int1,int2,int3){
+    let largest = int1;
+    if(int2>largest){
+        largest = int2;
+    }
+    if(int3> largest){
+        largest = int3;
+    }
+    return largest;
+}
 
+console.log(max(1,0,1));
+console.log(max(0,-10,-20));
+console.log(max(1000,510,440));
+
+//EXERCISE 3
