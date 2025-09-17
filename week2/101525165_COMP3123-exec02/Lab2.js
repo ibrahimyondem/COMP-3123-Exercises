1e341bba6e7bda9d29fcdb68408cb53f24a947bc
Ibrahim Yondem <ibrahim.y2001@gmail.com>
2025-09-16 20:30:28 -0400

    Lab Exercise 2

--- week2/101525165_COMP3123-exec02/Lab2.js
+++ week2/101525165_COMP3123-exec02/Lab2.js
@@ -1,13 +1,21 @@
 //COMP 3123 – Full Stack Development I – Lab 2
 //Exercise 1
 
-gretter = (myArray, counter) => {
-    let greetText = 'Hello';
+// gretter = (myArray, counter) => {
+//     let greetText = 'Hello';
 
-    for(let i of myArray){
-        console.log(`${greetText} ${i}`);
-    }
+//     for(let i of myArray){
+//         console.log(`${greetText} ${i}`);
+//     }
+// }
+
+// gretter(['Randy Savage', 'Ric Flair', 'Hulk Hogan'], 3);
+
+//Exercise 2
+function capitalize(string){
+    const [first, ...rest] = string.trim();
+    return first.toUpperCase() + rest.join('').toLowerCase();
 }
 
-gretter(['Randy Savage', 'Ric Flair', 'Hulk Hogan'], 3);
-
+console.log(capitalize("fooBar"));
+console.log(capitalize("nodeJs"));
