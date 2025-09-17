79f18d80abc2ea7aadf2a7cf8b26cbfe74ba8775
Ibrahim Yondem <ibrahim.y2001@gmail.com>
2025-09-16 20:48:56 -0400

    Lab Exercise 4

--- week2/101525165_COMP3123-exec02/Lab2.js
+++ week2/101525165_COMP3123-exec02/Lab2.js
@@ -1,6 +1,5 @@
 //COMP 3123 – Full Stack Development I – Lab 2
 //Exercise 1
-
 // gretter = (myArray, counter) => {
 //     let greetText = 'Hello';
 
@@ -11,19 +10,27 @@
 
 // gretter(['Randy Savage', 'Ric Flair', 'Hulk Hogan'], 3);
 
-//Exercise 2
-function capitalize(string){
-    const [first, ...rest] = string.trim();
-    return first.toUpperCase() + rest.join('').toLowerCase();
-}
+// //Exercise 2
+// function capitalize(string){
+//     const [first, ...rest] = string.trim();
+//     return first.toUpperCase() + rest.join('').toLowerCase();
+// }
 
-console.log(capitalize("fooBar"));
-console.log(capitalize("nodeJs"));
+// console.log(capitalize("fooBar"));
+// console.log(capitalize("nodeJs"));
 
-//Exercise 3
-const colors = ['red', 'green', 'blue'];
-const capitalizedColors = colors.map(capitalize);
-console.log(capitalizedColors);
+// //Exercise 3
+// const colors = ['red', 'green', 'blue'];
+// const capitalizedColors = colors.map(capitalize);
+// console.log(capitalizedColors);
 
 //Exercise 4
+var values = [1,60,34,30,20,5]
 
+const findNum = (num) => num < 20;
+
+const filterLessThan20 = values.filter(findNum);
+
+console.log(filterLessThan20);
+
+//Exercise 5
