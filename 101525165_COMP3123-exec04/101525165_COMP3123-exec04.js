f5b570de7efda0edb478033c5ebc23964c2b9826
Ibrahim Yondem <ibrahim.y2001@gmail.com>
2025-09-23 18:41:55 -0400

    New file

---
+++ 101525165_COMP3123-exec04/101525165_COMP3123-exec04.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.get('/user', (req, res) => {
+    const firstName = req.query.firstName || 'Pritesh';
+    const lastName = req.query.lastName || 'Patel';
+    res.json({ firstName, lastName });
+})
+
+app.post("/user/:firstname/:lastname", (req, res) => {
+  const { firstname, lastname } = req.params;
+  res.json({ firstname, lastname });
+});
+
+app.post("/users", (req, res) => {
+  const users = req.body; // array of objects [{firstname, lastname}, ...]
+  res.json(users);
+});
