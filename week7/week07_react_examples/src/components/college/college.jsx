e4b68e55f4773b9693ed91102219ab02c0036745
Ibrahim Yondem <ibrahim.y2001@gmail.com>
2025-10-14 18:22:19 -0400

    Lab Week 7 Finished

--- week7/week07_react_examples/src/components/college/college.jsx
+++ week7/week07_react_examples/src/components/college/college.jsx
@@ -5,6 +5,18 @@
 import React from 'react';
 
 class College extends React.Component{
+    constructor(props){
+        super();
+        this.props = {
+
+        }
+        
+        this.state = {
+            x: 100,
+            name: "Paritesh"
+        }
+    }
+1
     render(){
         return <h1 className='Text'>College: George Brown College</h1>;
     }
